cdcdc1d12faee093e8e004ea4355fe1d92c1b045
anr466 <anranr466@gmail.com>
2022-07-27 06:38:36 +0300

    up

--- stratgy.py
+++ stratgy.py
@@ -245,7 +245,7 @@ def hd():
     min_ = int(min_)
     sec_ = time.strftime("%S")
     sec_ = int(sec_)
-    for i in _15_mintue:
+    for i in five_minute:
         if min_ == i and sec_ == 3:
             ti.sleep(10)
             lunch()
