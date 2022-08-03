b01991a186435d3223ff4964da0a7a75206c1f56
anr466 <anranr466@gmail.com>
2022-08-03 09:19:58 +0300

    chat

--- stratgy.py
+++ stratgy.py
@@ -309,16 +309,16 @@ while True:
 
 
 
-# @bot.message_handler(func=lambda message: True)  
-# def t_mer(message):
-#     text = message.text
-#     chid = message.chat.id
-#     if text == "/start":
-#         bot.send_message(chid," تشغيل البوت")
-#     elif text == "/off":
-#         bot.send_message(chid," ايقاف البوت")
-#     else:
-#         bot.send_message(chid,"اشتغل")
+@bot.message_handler(func=lambda message: True)  
+def t_mer(message):
+    text = message.text
+    chid = message.chat.id
+    if text == "/start":
+        bot.send_message(chid," تشغيل البوت")
+    elif text == "/off":
+        bot.send_message(chid," ايقاف البوت")
+    else:
+        bot.send_message(chid,"اشتغل")
 
-# bot.infinity_polling()
+bot.infinity_polling()
     
