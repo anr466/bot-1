1c6882329337d2b016187a002d3ca49fbf719a1c
anr466 <anranr466@gmail.com>
2022-08-12 20:49:24 +0300

    t

--- stratgy.py
+++ stratgy.py
@@ -380,10 +380,10 @@ def track_price(t_tracking):
     exitbalance =signals.free_balance('balance')
     new_free_balance = exitbalance+buy_sell_balance
     signals.add_balance('balance', new_free_balance)
-    b = signals.free_balance('balance')
-    send_msg(f'balance is ===> {b}')
+    # b = signals.free_balance('balance')
+    # send_msg(f'balance is ===> {b}')
     
-    
+
 
     
 def summary():
@@ -415,7 +415,6 @@ def lunch():
 
 
 
-
 def hd():
     one_minute = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
     two_minute = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58]
@@ -433,25 +432,46 @@ def hd():
         if min_ == i and sec_ == 3:
             ti.sleep(5)
             lunch()
+
+
+@bot.message_handler(func=lambda message: True)
+def run():
+    while True:
+        hd()
+        def t_mer(message):
         
-while True:
-    hd()
-    
+            balance = signals.free_balance('balance')
+            text = message.text
+            text = text.lower()
+            chid = message.chat.id
+            if text == "bl":
+                bot.send_message(chid,f"balance is {balance}")
+            elif text == "sum":
+                bot.send_message(chid,f"{summary()}")
+        bot.polling()
 
 
-@bot.message_handler(func=lambda message: True)  
-def t_mer(message):
+
+
+# @bot.message_handler(func=lambda message: True)  
+# def t_mer(message):
     
-    balance = signals.free_balance('balance')
-    text = message.text
-    text = text.lower()
-    chid = message.chat.id
-    if text == "bl":
-        bot.send_message(chid,f"balance is {balance}")
-    elif text == "sum":
-        bot.send_message(chid,f"{summary()}")
-    return bot.polling()
-    
+#     balance = signals.free_balance('balance')
+#     text = message.text
+#     text = text.lower()
+#     chid = message.chat.id
+#     if text == "bl":
+#         bot.send_message(chid,f"balance is {balance}")
+#     elif text == "sum":
+#         bot.send_message(chid,f"{summary()}")
+#     return bot.polling()
+
+
+
+
+# Do some stuff ...
+
+
 
 
 # while True:
