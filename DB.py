32986de38fb7bd1d17028e5571f248d0a880b135
anr466 <anranr466@gmail.com>
2022-08-16 21:12:31 +0300

    h

--- DB.py
+++ DB.py
@@ -116,9 +116,9 @@ class signals:
 
 
 
-x = signals.free_balance('balance')
-# x= sum(x)
-print(x)
+# x = signals.free_balance('balance')
+# # x= sum(x)
+# print(x)
 
 
 # x= signals.find_all('buy')
